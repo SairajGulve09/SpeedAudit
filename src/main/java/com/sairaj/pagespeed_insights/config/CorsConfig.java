f678ba97ef81ce30957eaaf6859604608e561a33
Sairaj Gulve <130494632+SairajGulve09@users.noreply.github.com>
2025-04-05 15:16:08 +0530

    Update CorsConfig.java

--- src/main/java/com/sairaj/pagespeed_insights/config/CorsConfig.java
+++ src/main/java/com/sairaj/pagespeed_insights/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://speed-audit-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
